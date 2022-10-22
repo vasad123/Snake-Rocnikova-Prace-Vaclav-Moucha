2164eb678ac15a34494bf54c35fc69133e341985
Vašek Moucha <91481660+vasad123@users.noreply.github.com>
2022-10-22 20:00:26 +0200

    tried movement

--- res/js/main.js
+++ res/js/main.js
@@ -1,97 +1,98 @@
-
-const mybutton = document.getElementById("mybutton");  
-const wrapper= document.getElementById("wrapper");
+const mybutton = document.getElementById("mybutton");
+const wrapper = document.getElementById("wrapper");
 const footer = document.getElementById("footer");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 1380
+canvas.width = 1380;
 canvas.height = window.innerHeight;
 
-
-mybutton.onclick= () =>  {
-    wrapper.style.display ="none";
-    canvas.style.display ="flex";
-    footer.style.display ="none";
-   
+mybutton.onclick = () => {
+  wrapper.style.display = "none";
+  canvas.style.display = "flex";
+  footer.style.display = "none";
 };
-class myPlayer{
-    constructor(){
-        this.position ={
-            x:canvas.width/2,
-            y:500,
-        }
- 
-       this.width=50;
-        this.height=50
-        }
+class myPlayer {
+  constructor() {
+    this.position = {
+      x: canvas.width / 2,
+      y: 500,
+    };
+
+    this.width = 50;
+    this.height = 50;
+  }
+
+  draw() {
+    ctx.fillStyle = "green ";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  update() {
     
-        draw(){
-    ctx.fillStyle="green ";
-        ctx.fillRect(this.position.x,this.position.y,this.width,this.height)
-        }
-    update(){  
-        this.draw() 
- 
-    
-    }
-    
-    
-    }
+    this.draw();
+  }
+}
 
 const player = new myPlayer();
-  const keys ={
-right:{
-    pressed:false
-},
-left:{
-    pressed:false
-},
-up:{
-    pressed:false
-},
-down:{
-    pressed:false
-}
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+  up: {
+    pressed: false,
+  },
+  down: {
+    pressed: false,
+  },
+};
 
+function animation() {
+  requestAnimationFrame(animation);
+  player.update();
+  if (keys.left.pressed) {
+    console.log("move to left");
+    player.position.x = player.position.x - 5;
+    keys.up.pressed = false;
+    keys.right.pressed = false;
+    keys.down.pressed = false;
   }
-
-function animation(){
-    requestAnimationFrame(animation)
-    ctx.clearRect(0, 0, canvas.width, canvas.height)
-  
-    player.update(); 
-    if(keys.left.pressed){
- player.position.x= player.position.x-5;
-
-  } if(keys.up.pressed){
-   
- 
-player.position.y= player.position.y-5;
-  } if(keys.down.pressed){
-    player.position.y= player.position.y+5;
-
-  
-  } if(keys.right.pressed){
-    player.position.x =  player.position.x+5;
- 
+  if (keys.up.pressed) {
+    keys.down.pressed = false;
+    keys.left.pressed = false;
+    keys.right.pressed = false;
+    console.log("move up");
+    player.position.y = player.position.y - 5;
   }
-
+  if (keys.down.pressed) {
+    player.position.y = player.position.y + 5;
+    console.log("move down");
+    keys.up.pressed = false;
+    keys.left.pressed = false;
+    keys.right.pressed = false;
+  }
+  if (keys.right.pressed) {
+    console.log("move to right");
+    player.position.x = player.position.x + 5;
+    keys.left.pressed = false;
+    keys.up.pressed = false;
+    keys.down.pressed = false;
+  }
 }
 animation();
-addEventListener('keydown',({keyCode}) =>{
-    switch(keyCode){
-    case 65:    
-    keys.left.pressed=true
-    break
-    case 68 :   
-    keys.right.pressed=true
-    break
-    case 87:    
-   keys.up.pressed = true
-    break
+addEventListener("keydown", ({ keyCode }) => {
+  switch (keyCode) {
+    case 65:
+      keys.left.pressed = true;
+      break;
+    case 68:
+      keys.right.pressed = true;
+      break;
+    case 87:
+      keys.up.pressed = true;
+      break;
     case 83:
-    keys.down.pressed=true
-    
-    
-   } })
-  
+      keys.down.pressed = true;
+  }
+});
