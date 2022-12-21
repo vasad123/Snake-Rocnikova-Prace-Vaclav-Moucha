aec6fb9bb077caaad1210f78f21dfda4becbc5f1
vasad123 <you@example.com>
2022-12-21 10:58:28 +0100

    brought back code from past cuz i screw up

--- res/js/main.js
+++ res/js/main.js
@@ -1,15 +1,24 @@
-import myPlayer from "./player.js";
+
 import {
   mybutton,
   wrapper,
   footer,
   canvas,
   ctx,
-  Directions,
+
 } from "./globalContext.js";
-import { Fruit } from "./fruit.js";
-import { keys, animation } from "./movement.js";
-let startMenuOff;
+
+canvas.width = 1380;
+canvas.height = window.innerHeight;
+let startMenuOff = false;
+const Directions = {
+  directionLeft: 1,
+  directionRight: 2,
+  directionUp: 3,
+  directionDown: 4,
+};
+let CurrentDirection;
+
 
 mybutton.onclick = () => {
   wrapper.style.display = "none";
@@ -17,9 +26,166 @@ mybutton.onclick = () => {
   footer.style.display = "none";
   startMenuOff = true;
 };
+class myPlayer {
+  constructor() {
+    this.position = {
+      x: canvas.width / 2,
+      y: 500,
+    };
 
+    this.width = 50;
+    this.height = 50;
+  }
+
+  draw() {
+    ctx.fillStyle = "green";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  update() {
+    this.draw();
+  }
+}
+class Fruit {
+  constructor() {
+    this.position = {
+      x: Math.random() * canvas.width,
+      y: Math.random() * canvas.height,
+    };
+
+    this.width = 30;
+    this.height = 30;
+  }
+
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+  update() {
+    this.draw();
+  }
+}
 const fruit = new Fruit();
 const player = new myPlayer();
+
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+  up: {
+    pressed: false,
+  },
+  down: {
+    pressed: false,
+  },
+};
+
+function animation() {
+  console.log("DIRRRR" + Directions.left);
+  requestAnimationFrame(animation);
+ 
+ctx.clearRect(0,0,canvas.width,canvas.height);
+  if (
+    fruit.position.x == player.position.x &&
+    fruit.position.y == player.position.y
+  ) {
+    fruit.position.x = Math.random() * canvas.width;
+    fruit.position.y = Math.random() * canvas.height;
+    fruit.update();
+  } 
+  fruit.update();
+  player.update();
+
+  if (keys.left.pressed) {
+    if (CurrentDirection !== Directions.directionRight) {
+      player.position.x = player.position.x - 5;
+      CurrentDirection = Directions.directionLeft;
+    } else {
+      keys.right.pressed = true;
+    }
+  }
+  if (keys.up.pressed) {
+    if (CurrentDirection !== Directions.directionDown) {
+      player.position.y = player.position.y - 5;
+      CurrentDirection = Directions.directionUp;
+    } else {
+      keys.down.pressed = true;
+    }
+  }
+  if (keys.down.pressed) {
+    if (CurrentDirection !== Directions.directionUp) {
+      player.position.y = player.position.y + 5;
+      CurrentDirection = Directions.directionDown;
+    } else {
+      keys.up.pressed = true;
+    }
+  }
+  if (keys.right.pressed) {
+    if (CurrentDirection !== Directions.directionLeft) {
+      player.position.x = player.position.x + 5;
+      CurrentDirection = Directions.directionRight;
+    } else {
+      keys.left.pressed = true;
+    }
+  }
+  if (player.position.x >= canvas.width) {
+    player.position.x = 0;
+  } else if (player.position.x < 0) {
+    player.position.x = canvas.width;
+  } else if (player.position.y >= canvas.height) {
+    player.position.y = 0;
+  } else if (player.position.y < 0) {
+    player.position.y = window.innerHeight;
+  }
+}
+
+addEventListener("keydown", ({ keyCode }) => {
+  switch (keyCode) {
+    case 65:
+      keys.left.pressed = true;
+      keys.up.pressed = false;
+      keys.right.pressed = false;
+      keys.down.pressed = false;
+      if (CurrentDirection === null) {
+        CurrentDirection = Directions.directionLeft;
+      }
+
+      break;
+    case 68:
+      keys.right.pressed = true;
+      keys.left.pressed = false;
+      keys.up.pressed = false;
+      keys.down.pressed = false;
+      if (CurrentDirection === null) {
+        CurrentDirection = Directions.directionRight;
+      }
+      break;
+    case 87:
+      keys.up.pressed = true;
+      keys.down.pressed = false;
+      keys.left.pressed = false;
+      keys.right.pressed = false;
+      if (CurrentDirection === null) {
+        CurrentDirection = Directions.directionUp;
+      }
+      break;
+    case 83:
+      keys.down.pressed = true;
+      keys.up.pressed = false;
+      keys.left.pressed = false;
+      keys.right.pressed = false;
+      if (CurrentDirection === null) {
+        CurrentDirection = Directions.directionDown;
+      }
+
+      break;
+  }
+});
+
+
+
 
 fruit.update();
 
